5626a5569d9d7a0cbbcc4e5d62f9aedea734071d
TeeboiCode <oluwaleyetaiwo5228@gmail.com>
2024-10-31 08:05:30 +0100

    Added db.json, update App vue, update Admin Home page - added preloading component, added Admin Login view

---
+++ src/views/AdminLoginView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container-fluid">
+    <AdminLogin />
+  </div>
+</template>
+
+<script>
+import AdminLogin from "../components/Admin/AdminPages/AdminLogin.vue";
+export default {
+  name: "AdminLogiView",
+  components: {
+    AdminLogin,
+  },
+};
+</script>
+
+<style></style>
